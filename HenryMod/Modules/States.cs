786bb120020df29cb12ae1faa16869403c950380
Ethan Goh <35375774+Ethanol10@users.noreply.github.com>
2022-05-23 13:03:13 +1000

    Initial commit

---
+++ HenryMod/Modules/States.cs
@@ -0,0 +1,22 @@
+﻿using HenryMod.SkillStates;
+using HenryMod.SkillStates.BaseStates;
+using System.Collections.Generic;
+using System;
+
+namespace HenryMod.Modules
+{
+    public static class States
+    {
+        internal static void RegisterStates()
+        {
+            Modules.Content.AddEntityState(typeof(BaseMeleeAttack));
+            Modules.Content.AddEntityState(typeof(SlashCombo));
+
+            Modules.Content.AddEntityState(typeof(Shoot));
+
+            Modules.Content.AddEntityState(typeof(Roll));
+
+            Modules.Content.AddEntityState(typeof(ThrowBomb));
+        }
+    }
+}
