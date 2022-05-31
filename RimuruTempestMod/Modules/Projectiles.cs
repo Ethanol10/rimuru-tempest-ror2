1bcbd0c8a8d6ddf22e8a2b2ed64ca2e6a0b639ad
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2022-05-31 21:14:08 +1000

    POC for waterblade

--- RimuruTempestMod/Modules/Projectiles.cs
+++ RimuruTempestMod/Modules/Projectiles.cs
@@ -9,6 +9,7 @@ namespace RimuruMod.Modules
     internal static class Projectiles
     {
         internal static GameObject bombPrefab;
+        internal static GameObject waterbladeProjectile;
 
         internal static void RegisterProjectiles()
         {
@@ -42,21 +43,53 @@ namespace RimuruMod.Modules
             bombController.startSound = "";
         }
 
-        private static void InitializeImpactExplosion(ProjectileImpactExplosion projectileImpactExplosion)
+        private static void CreateWaterBlade() 
         {
-            projectileImpactExplosion.blastDamageCoefficient = 1f;
-            projectileImpactExplosion.blastProcCoefficient = 1f;
-            projectileImpactExplosion.blastRadius = 1f;
+            waterbladeProjectile = Modules.Assets.mainAssetBundle.LoadAsset<GameObject>("WaterBlade");
+            Modules.Prefabs.SetupHitbox(waterbladeProjectile, waterbladeProjectile.transform.GetChild(1), "waterblade");
+            waterbladeProjectile.AddComponent<NetworkIdentity>();
+            ProjectileController waterbladeProjectileCon = waterbladeProjectile.AddComponent<ProjectileController>();
+            ProjectileDamage waterbladeProjectileDamage = waterbladeProjectile.AddComponent<ProjectileDamage>();
+            ProjectileImpactExplosion waterbladeProjectileImpactExplosion = waterbladeProjectile.AddComponent<ProjectileImpactExplosion>();
+            Modules.Projectiles.InitializeImpactExplosion(waterbladeProjectileImpactExplosion);
+
+            //Waterblade Damage
+            waterbladeProjectileCon.procCoefficient = 1.0f;
+
+            PrefabAPI.RegisterNetworkPrefab(waterbladeProjectile);
+
+
+            /*
+                Creating projectile:
+                - needs ProjectileController
+                - needs projectileOverlapAttack
+                
+             */
+        }
+
+        internal static void InitializeWaterBladeDamage(ProjectileDamage damageComponent) 
+        {
+            damageComponent.damage = Modules.StaticValues.waterbladeDamageCoefficient;
+            damageComponent.crit = false;
+            damageComponent.force = Modules.StaticValues.waterbladeForce;
+            damageComponent.damageType = DamageType.Generic;
+        }
+
+        internal static void InitializeImpactExplosion(ProjectileImpactExplosion projectileImpactExplosion)
+        {
+            projectileImpactExplosion.blastDamageCoefficient = 0f;
+            projectileImpactExplosion.blastProcCoefficient = 0f;
+            projectileImpactExplosion.blastRadius = 0f;
             projectileImpactExplosion.bonusBlastForce = Vector3.zero;
             projectileImpactExplosion.childrenCount = 0;
             projectileImpactExplosion.childrenDamageCoefficient = 0f;
             projectileImpactExplosion.childrenProjectilePrefab = null;
             projectileImpactExplosion.destroyOnEnemy = false;
-            projectileImpactExplosion.destroyOnWorld = false;
+            projectileImpactExplosion.destroyOnWorld = true;
             projectileImpactExplosion.falloffModel = RoR2.BlastAttack.FalloffModel.None;
             projectileImpactExplosion.fireChildren = false;
             projectileImpactExplosion.impactEffect = null;
-            projectileImpactExplosion.lifetime = 0f;
+            projectileImpactExplosion.lifetime = 10f;
             projectileImpactExplosion.lifetimeAfterImpact = 0f;
             projectileImpactExplosion.lifetimeRandomOffset = 0f;
             projectileImpactExplosion.offsetForLifetimeExpiredSound = 0f;
--- RimuruTempestMod/Modules/StaticValues.cs
+++ RimuruTempestMod/Modules/StaticValues.cs
@@ -4,12 +4,6 @@ namespace RimuruMod.Modules
 {
     internal static class StaticValues
     {
-        internal static string descriptionText = "Rimuru is a skilled fighter who makes use of a wide arsenal of weaponry to take down his foes.<color=#CCD3E0>" + Environment.NewLine + Environment.NewLine
-             + "< ! > Sword is a good all-rounder while Boxing Gloves are better for laying a beatdown on more powerful foes." + Environment.NewLine + Environment.NewLine
-             + "< ! > Pistol is a powerful anti air, with its low cooldown and high damage." + Environment.NewLine + Environment.NewLine
-             + "< ! > Roll has a lingering armor buff that helps to use it aggressively." + Environment.NewLine + Environment.NewLine
-             + "< ! > Bomb can be used to wipe crowds with ease." + Environment.NewLine + Environment.NewLine;
-
         internal const float swordDamageCoefficient = 2.8f;
 
         internal const float gunDamageCoefficient = 4.2f;
@@ -18,5 +12,10 @@ namespace RimuruMod.Modules
 
         internal const int spatialmovementbuffDuration = 3;
         internal const int analyzedebuffDuration = 6;
+
+        //Waterblade parameters
+        internal const float waterbladeDamageCoefficient = 3.0f;
+        internal const float waterbladeForce = 100f;
+        
     }
 }
