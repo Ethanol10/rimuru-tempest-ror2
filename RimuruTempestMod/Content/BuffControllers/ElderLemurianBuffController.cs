096c85b0194940307c09bcef99f2905285940486
Ethan Goh <ethangoh98@gmail.com>
2023-02-28 11:34:06 +1100

    base buff class

---
+++ RimuruTempestMod/Content/BuffControllers/ElderLemurianBuffController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using RoR2;
+using On.RoR2;
+using RimuruMod.Modules;
+using System.Reflection;
+
+namespace RimuruTempestMod.Content.BuffControllers
+{
+    /*
+     Elder Lemurian
+     Effect: Strengthen Body - Damage x 1.5
+     */
+
+    public class ElderLemurianBuffController : RimuruBaseBuffController
+    {
+        public override void Awake()
+        {
+            base.Awake();
+            isPermaBuff = false;
+            lifetime = 10f;
+            //Apply strengthBuff here?
+
+        }
+
+        public void Hook()
+        {
+            On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
+        }
+
+        public void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, RoR2.CharacterBody self)
+        {
+            orig(self);
+            if (self)
+            {
+                if (self.HasBuff(RimuruMod.Modules.Buffs.strengthBuff))
+                {
+                    self.damage *= StaticValues.strengthBuffCoefficient;
+                }
+            }
+        }
+    }
+}
+
---
+++ RimuruTempestMod/Content/BuffControllers/RimuruBaseBuffController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace RimuruTempestMod.Content.BuffControllers
+{
+    /*
+     All Buffs should inherit from this.
+     This should apply on the Master gameObject, not the body object.
+     */
+
+	public class RimuruBaseBuffController : MonoBehaviour
+	{
+        public bool isSkillOverride = false;
+        public bool isPermaBuff = false;
+        public float stopwatch;
+        public float lifetime;
+
+        public virtual void Awake()
+        {
+            ApplySkillChange();
+            stopwatch = 0f;
+        }
+
+        public virtual void FixedUpdate()
+        {
+            ActiveBuffEffect();
+            stopwatch += Time.fixedDeltaTime;
+
+            if (stopwatch >= lifetime && !isPermaBuff)
+            {
+                Destroy(this);
+            }
+        }
+
+        public virtual void ActiveBuffEffect()
+        {
+
+        }
+
+        public virtual void ApplySkillChange()
+        {
+            //Apply skill overrides here.
+        }
+    }
+}
+
