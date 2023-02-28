6c1e6ae265e1f26d0d14ad981cb2b135f21d97f0
Ethan Goh <ethangoh98@gmail.com>
2023-02-28 13:39:49 +1100

    Elder Lem example

--- RimuruTempestMod/Content/BuffControllers/ElderLemurianBuffController.cs
+++ RimuruTempestMod/Content/BuffControllers/ElderLemurianBuffController.cs
@@ -4,6 +4,7 @@ using RoR2;
 using On.RoR2;
 using RimuruMod.Modules;
 using System.Reflection;
+using R2API.Networking;
 
 namespace RimuruTempestMod.Content.BuffControllers
 {
@@ -14,18 +15,37 @@ namespace RimuruTempestMod.Content.BuffControllers
 
     public class ElderLemurianBuffController : RimuruBaseBuffController
     {
+        public RoR2.CharacterBody body;
+
         public override void Awake()
         {
             base.Awake();
             isPermaBuff = false;
             lifetime = 10f;
-            //Apply strengthBuff here?
+        }
 
+        public void Start()
+        {
+            body = gameObject.GetComponent<RoR2.CharacterMaster>().GetBody();
+
+            if (body)
+            {
+                body.ApplyBuff(Buffs.strengthBuff.buffIndex, 1, -1);
+            }
         }
 
         public void Hook()
         {
             On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
+        }
+
+        public void OnDestroy()
+        {
+            //Unapply StrengthBuff here?
+            if (body)
+            {
+                body.RemoveBuff(Buffs.strengthBuff.buffIndex);
+            }
         }
 
         public void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, RoR2.CharacterBody self)
