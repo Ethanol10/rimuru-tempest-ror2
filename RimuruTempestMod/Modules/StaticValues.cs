1d8ee82f4b1985fa4dfd05ff2545a5a4a0f77e76
1TeaL <soxas.roxas@gmail.com>
2023-03-14 18:15:10 +1100

    initial dic

--- RimuruTempestMod/Modules/StaticValues.cs
+++ RimuruTempestMod/Modules/StaticValues.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RimuruMod.Modules.Survivors;
+using RimuruTempestMod.Content.BuffControllers;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace RimuruMod.Modules
 {
@@ -47,5 +51,69 @@ namespace RimuruMod.Modules
         internal const float lemurianfireDamageCoefficient = 4f;
         internal const float lemurianfireProcCoefficient = 1f;
         internal const float lemurianfireRadius = 15f;
+
+
+        public static Dictionary<string, RimuruBaseBuffController> rimDic;
+
+        public static void LoadDictionary()
+        {
+            ElderLemurianBuffController elderLemCon = new ElderLemurianBuffController();
+
+            rimDic = new Dictionary<string, RimuruBaseBuffController>();
+            rimDic.Add("MinorConstructBody", elderLemCon);
+            rimDic.Add("MinorConstructOnKillBody", IndicatorType.PASSIVE);
+            rimDic.Add("BeetleBody", IndicatorType.PASSIVE);
+            rimDic.Add("FlyingVerminBody", IndicatorType.PASSIVE);
+            rimDic.Add("VerminBody", IndicatorType.PASSIVE);
+            rimDic.Add("GupBody", IndicatorType.PASSIVE);
+            rimDic.Add("GipBody", IndicatorType.PASSIVE);
+            rimDic.Add("GeepBody", IndicatorType.PASSIVE);
+            rimDic.Add("HermitCrabBody", IndicatorType.PASSIVE);
+            rimDic.Add("AcidLarvaBody", IndicatorType.PASSIVE);
+            rimDic.Add("WispBody", IndicatorType.PASSIVE);
+            rimDic.Add("LunarExploderBody", IndicatorType.PASSIVE);
+            rimDic.Add("MiniMushroomBody", IndicatorType.PASSIVE);
+            rimDic.Add("RoboBallMiniBody", IndicatorType.PASSIVE);
+            rimDic.Add("RoboBallGreenBuddyBody", IndicatorType.PASSIVE);
+            rimDic.Add("RoboBallRedBuddyBody", IndicatorType.PASSIVE);
+            rimDic.Add("VoidBarnacleBody", IndicatorType.PASSIVE);
+            rimDic.Add("VoidJailerBody", IndicatorType.PASSIVE);
+            rimDic.Add("ImpBossBody", IndicatorType.PASSIVE);
+            rimDic.Add("TitanBody", IndicatorType.PASSIVE);
+            rimDic.Add("TitanGoldBody", IndicatorType.PASSIVE);
+            rimDic.Add("VagrantBody", IndicatorType.PASSIVE);
+            rimDic.Add("MagmaWormBody", IndicatorType.PASSIVE);
+            rimDic.Add("ElectricWormBody", IndicatorType.PASSIVE);
+            rimDic.Add("CaptainBody", IndicatorType.PASSIVE);
+            rimDic.Add("CommandoBody", IndicatorType.PASSIVE);
+            rimDic.Add("CrocoBody", IndicatorType.PASSIVE);
+            rimDic.Add("LoaderBody", IndicatorType.PASSIVE);
+
+            rimDic.Add("VultureBody", IndicatorType.ACTIVE);
+            rimDic.Add("BeetleGuardBody", IndicatorType.ACTIVE);
+            rimDic.Add("BisonBody", IndicatorType.ACTIVE);
+            rimDic.Add("BellBody", IndicatorType.ACTIVE);
+            rimDic.Add("ClayGrenadierBody", IndicatorType.ACTIVE);
+            rimDic.Add("ClayBruiserBody", IndicatorType.ACTIVE);
+            rimDic.Add("LemurianBruiserBody", IndicatorType.ACTIVE);
+            rimDic.Add("GreaterWispBody", IndicatorType.ACTIVE);
+            rimDic.Add("ImpBody", IndicatorType.ACTIVE);
+            rimDic.Add("JellyfishBody", IndicatorType.ACTIVE);
+            rimDic.Add("LemurianBody", IndicatorType.ACTIVE);
+            rimDic.Add("LunarGolemBody", IndicatorType.ACTIVE);
+            rimDic.Add("LunarWispBody", IndicatorType.ACTIVE);
+            rimDic.Add("ParentBody", IndicatorType.ACTIVE);
+            rimDic.Add("GolemBody", IndicatorType.ACTIVE);
+            rimDic.Add("NullifierBody", IndicatorType.ACTIVE);
+            rimDic.Add("BeetleQueen2Body", IndicatorType.ACTIVE);
+            rimDic.Add("GravekeeperBody", IndicatorType.ACTIVE);
+            rimDic.Add("ClayBossBody", IndicatorType.ACTIVE);
+            rimDic.Add("GrandParentBody", IndicatorType.ACTIVE);
+            rimDic.Add("RoboBallBossBody", IndicatorType.ACTIVE);
+            rimDic.Add("SuperRoboBallBossBody", IndicatorType.ACTIVE);
+            rimDic.Add("MegaConstructBody", IndicatorType.ACTIVE);
+            rimDic.Add("VoidMegaCrabBody", IndicatorType.ACTIVE);
+        }
+
     }
 }
