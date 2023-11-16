9e3873e5991c566d03bef06df02179730c644c14
1TeaL <soxas.roxas@gmail.com>
2023-11-16 21:42:29 +1100

    adding damage type

---
+++ RimuruTempestMod/Modules/Damage.cs
@@ -0,0 +1,17 @@
+﻿using R2API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShiggyMod.Modules
+{
+    public static class Damage
+    {
+        internal static DamageAPI.ModdedDamageType shiggyDecay;
+
+        internal static void SetupModdedDamage()
+        {
+            shiggyDecay = DamageAPI.ReserveDamageType();
+        }
+    }
+}
