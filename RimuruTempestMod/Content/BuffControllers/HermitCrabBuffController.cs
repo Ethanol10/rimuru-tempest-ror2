73e6a89c8adbe15e5daf66c430070b3fd85f59d8
1TeaL <soxas.roxas@gmail.com>
2023-11-16 21:25:00 +1100

    teammask all

--- RimuruTempestMod/Content/BuffControllers/HermitCrabBuffController.cs
+++ RimuruTempestMod/Content/BuffControllers/HermitCrabBuffController.cs
@@ -174,7 +174,7 @@ namespace RimuruTempestMod.Content.BuffControllers
             BullseyeSearch bullseyeSearch = new BullseyeSearch();
             bullseyeSearch.searchOrigin = position;
             bullseyeSearch.searchDirection = Vector3.zero;
-            bullseyeSearch.teamMaskFilter = TeamMask.allButNeutral;
+            bullseyeSearch.teamMaskFilter = TeamMask.all;
             bullseyeSearch.teamMaskFilter.RemoveTeam(this.teamIndex);
             bullseyeSearch.filterByLoS = false;
             bullseyeSearch.sortMode = BullseyeSearch.SortMode.Distance;
