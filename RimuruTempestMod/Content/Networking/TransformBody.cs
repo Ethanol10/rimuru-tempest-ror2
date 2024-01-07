74adc0ed62baac53fe29b156a853294198cc214a
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2024-01-08 01:42:10 +1100

    attempting body transform fix.

--- RimuruTempestMod/Content/Networking/TransformBody.cs
+++ RimuruTempestMod/Content/Networking/TransformBody.cs
@@ -2,6 +2,7 @@
 using RoR2;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.TextCore;
 
 
 namespace RimuruMod.Modules.Networking
@@ -71,12 +72,51 @@ namespace RimuruMod.Modules.Networking
                         break;
                 }
 
+                GameObject newBody = BodyCatalog.FindBodyPrefab(selectedBody);
+
+
                 //Attempt transform
-                characterMaster.TransformBody(selectedBody);
+                //characterMaster.TransformBody(selectedBody);
+
+                StupidExternalSpawningBodyFunction(characterMaster, newBody);
 
                 //Perform Finalization on all clients.
                 new UpdateControllers(netID, health).Send(R2API.Networking.NetworkDestination.Clients);
             }
+        }
+
+        public CharacterBody StupidExternalSpawningBodyFunction(CharacterMaster master, GameObject bodyPrefab) 
+        {
+            Vector3 footPosition = master.GetBody().transform.position;
+            Quaternion rotation = master.GetBody().transform.rotation;
+
+            //Destroy body first.
+            master.DestroyBody();
+
+            //Do spawning thing. This was ripped from Respawn(), but we didn't want to use the metamorphisis stuff so this is why it's a clone.
+            if (bodyPrefab)
+            {
+                CharacterBody component = bodyPrefab.GetComponent<CharacterBody>();
+                if (component)
+                {
+                    Vector3 position = footPosition;
+                    if (true)
+                    {
+                        position = master.CalculateSafeGroundPosition(footPosition, component);
+                    }
+                    return master.SpawnBody(position, rotation);
+                }
+                Debug.LogErrorFormat("Trying to respawn as object {0} who has no Character Body!", new object[]
+                {
+                    bodyPrefab
+                });
+            }
+            else 
+            {
+                Debug.Log("LMAO failed to spawn.");
+            }
+
+            return null;
         }
     }
 }
